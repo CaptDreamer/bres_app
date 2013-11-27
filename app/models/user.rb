7b5247ad2240d4bcfd5cbd269df4beafb5d7c296
Amit Bader <bader.amit@gmail.com>
2013-11-26 20:01:32 -0600

    finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
   	before_save { email.downcase! }
-	validates :name, presence: true, length: { maximum: 50 }
+	validates :name, presence: true, length: { maximum: 50 }, inclusion: { in: ['Amit Bader', 'Bre Bader'], message: "Sorry, %{value} is not authorized to sign up at this time" }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 BresApp::Application.routes.draw do
+  resources :users
   root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
