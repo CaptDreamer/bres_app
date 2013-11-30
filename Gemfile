2bdf2f127952ccbb77c973da4c83010aeef5209e
Amit Bader <capt.dreamer@gmail.com>
2013-11-30 15:57:36 -0600

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'bootstrap-sass', '~> 3.0.2.0'
 gem 'bcrypt-ruby', '3.1.2'
 gem 'faker', '1.1.2'
 gem 'will_paginate', '3.0.4'
-gem 'bootstrap-will_paginate', '0.0.9'
+gem 'will_paginate-bootstrap', '1.0.0'
 gem 'holder_rails', '2.2.0'
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
   	before_save { email.downcase! }
   	before_create :create_remember_token
-	validates :name, presence: true, length: { maximum: 50 }, inclusion: { in: ['Amit Bader', 'Bre Bader'], message: "Sorry, %{value} is not authorized to sign up at this time" }
+	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
